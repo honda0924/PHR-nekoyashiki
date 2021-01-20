3ca5f5a33a8b2d23f48a278f2eb081d060cd0672
nekoyashiki <47511135+honda0924@users.noreply.github.com>
2021-01-20 20:19:05 +0900

    add_vital_controller_files

---
+++ app/controllers/vitals_controller.rb
@@ -0,0 +1,20 @@
+class VitalsController < ApplicationController
+  def index
+    
+  end
+  def new
+    
+  end
+  def create
+    
+  end
+  def edit
+    
+  end
+  def update
+    
+  end
+  def destroy
+    
+  end
+end
